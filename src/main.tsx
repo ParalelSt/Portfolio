d9407168f582ebe0fef05271ba5823f3bf0da6f2
AronM <strixparalel@gmail.com>
2024-08-28 14:19:19 +0200

    V5

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./Global/GlobalStyles.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router basename={"portfolio/"}>
+    <Router>
       <App />
     </Router>
   </StrictMode>
