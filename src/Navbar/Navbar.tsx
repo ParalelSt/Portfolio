6bea92ff613bb9f0b542cf9ef4b39c4070183156
AronM <strixparalel@gmail.com>
2024-08-29 13:09:35 +0200

    v18

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Navbar.scss";
+import { FaBars } from "react-icons/fa6";
 
 function Navbar() {
   const name = "Aron Matoić";
@@ -29,6 +30,25 @@ function Navbar() {
             </Link>
           </div>
           <div className="nav-item">
+            <Link to={"/portfolio/contact"}>
+              <h1>Contact</h1>
+            </Link>
+          </div>
+          <div className="nav-item drop-down-btn">
+            <FaBars size={20}></FaBars>
+          </div>
+        </div>
+        <div className="drop-down-container">
+          <div className="nav-item-container">
+            <Link to={"/portfolio/"}>
+              <h1>Home</h1>
+            </Link>
+            <Link to={"/portfolio/projects"}>
+              <h1>Projects</h1>
+            </Link>
+            <Link to={"/portfolio/about-me"}>
+              <h1>About me</h1>
+            </Link>
             <Link to={"/portfolio/contact"}>
               <h1>Contact</h1>
             </Link>
