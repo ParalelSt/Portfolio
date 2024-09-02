87a7b8d14a0dd830f45ea90d24013f47306a8bc9
AronM <strixparalel@gmail.com>
2024-09-02 17:13:17 +0200

    v23

--- src/Content/Projects/Projects.tsx
+++ src/Content/Projects/Projects.tsx
@@ -14,51 +14,10 @@ function Projects() {
             <div className="img-container">
               <img src="./images/amazontypescript.png" />
             </div>
-            <div className="title">Amazon Copy</div>
+            <div className="title">Amazon Clone</div>
             <div className="description">
-              Lorem, ipsum da dolor sit amet consectetur adipisicing elit.
-              Aliquid amet quas magnam error doloremque quae illum id. Nisi
-              doloribus molestiae enim non magnam iste dolore voluptate illum.
-              Omnis, magnam cupiditate.
-            </div>
-          </Link>
-
-          <Link onClick={redirect} to={""} className="project-container">
-            <div className="img-container">
-              <img src="./images/amazontypescript.png" />
-            </div>
-            <div className="title">Amazon Copy</div>
-            <div className="description">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              amet quas magnam error doloremque quae illum id. Nisi doloribus
-              molestiae enim non magnam iste dolore voluptate illum. Omnis,
-              magnam cupiditate.
-            </div>
-          </Link>
-
-          <Link onClick={redirect} to={""} className="project-container">
-            <div className="img-container">
-              <img src="./images/amazontypescript.png" />
-            </div>
-            <div className="title">Amazon Copy</div>
-            <div className="description">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              amet quas magnam error doloremque quae illum id. Nisi doloribus
-              molestiae enim non magnam iste dolore voluptate illum. Omnis,
-              magnam cupiditate.
-            </div>
-          </Link>
-
-          <Link onClick={redirect} to={""} className="project-container">
-            <div className="img-container">
-              <img src="./images/amazontypescript.png" />
-            </div>
-            <div className="title">Amazon Copy</div>
-            <div className="description">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              amet quas magnam error doloremque quae illum id. Nisi doloribus
-              molestiae enim non magnam iste dolore voluptate illum. Omnis,
-              magnam cupiditate.
+              Amazon clone that I made using React.js, styled-components and
+              Typescript
             </div>
           </Link>
         </div>
